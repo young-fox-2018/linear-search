36731a4849f2bea15e8a55aa2f13c75f6663a70f
javasusanto <herisusanto.id@gmail.com>
2018-10-30 13:57:03 +0700

    Linear Search

--- linear_search.js
+++ linear_search.js
@@ -4,9 +4,15 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  let index = []
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === target) index.push(i)
+  }
+  if (index.length === 0) return -1
+  return index[0]
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37];
 
 console.log(linearSearch(18, random_numbers));
 // 2
@@ -17,6 +23,11 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let result = []
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === target) result.push(i)
+  }
+  return result
 }
 
 let banana_arr = "banana".split("");
