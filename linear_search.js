95fe7815b803443a5b8424ef4c7c1ae2df34a720
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-10-30 11:25:54 +0700

    done

--- linear_search.js
+++ linear_search.js
@@ -4,9 +4,15 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  let isFind = []
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === target) isFind.push(i)
+  }
+  if (isFind.length === 0) return -1
+  else return isFind[0]
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37];
 
 console.log(linearSearch(18, random_numbers));
 // 2
@@ -16,7 +22,12 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  let isFind = []
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === target) isFind.push(i)
+  }
+  if (isFind.length === 0) return -1
+  else return isFind
 }
 
 let banana_arr = "banana".split("");
