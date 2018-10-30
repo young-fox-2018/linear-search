32263b5d15218d3b8494dc1ec2aaa7c8da9e11e9
imanuel vicky <imanuvi@imanuels-MBP.localdomain>
2018-10-30 11:41:21 +0700

    done

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,11 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] == target) {
+        return i
+    }
+  } return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +20,13 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  let output = []
+  for (let i = 0; i < values.length; i++) {
+      if (values[i] == target) {
+        output.push(i)
+      }
+  } if (output.length > 0) return output
+  return -1
 }
 
 let banana_arr = "banana".split("");
