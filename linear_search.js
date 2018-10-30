7d6a592904d5edfa63b927214a6a0bf9b8124c40
vinutama <muhammadkhevin@gmail.com>
2018-10-30 11:19:05 +0700

    linear search done

--- linear_search.js
+++ linear_search.js
@@ -3,10 +3,11 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  let indexMatch = values.indexOf(target)
+  return indexMatch
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37];
 
 console.log(linearSearch(18, random_numbers));
 // 2
@@ -16,7 +17,13 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  let indexArr = []
+  for (let i = 0; i < values.length; i++) {
+    if (target === values[i]) {
+      indexArr.push(i)
+    }
+  }
+  return indexArr
 }
 
 let banana_arr = "banana".split("");
