64d86ad377c19070129a688bdcef7f30c2e67025
ranggakusuma <ranggavskusuma@gmail.com>
2018-10-30 11:50:09 +0700

    linear search done

--- linear_search.js
+++ linear_search.js
@@ -3,8 +3,14 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  for(var i=0;i<values.length;i++){
+    if (target == values[i]) {
+      return i 
+    } 
+  }
+  return -1
 }
+
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 
@@ -13,10 +19,19 @@ console.log(linearSearch(18, random_numbers));
 console.log(linearSearch(9, random_numbers));
 // -1
 
-// Release 1
+// // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  var temp = []
+  var finded = false
+  for( var i = 0; i<values.length; i++) { 
+    finded = false
+    if(target === values[i]){
+      temp.push(i)
+    }
+  }
+
+  return temp
 }
 
 let banana_arr = "banana".split("");
