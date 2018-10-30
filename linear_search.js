33a745e47ae1d1c24a98043564866c251bad1ac9
abednegolubis <lubisabednego@gmail.com>
2018-10-30 17:49:09 +0700

    submit challenge : linear search

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,12 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  var result = 0
+  for (let i = 0; i < values.length; i++) {
+    if(values[i] === target)
+      return   i
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +21,18 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+    var result = []
+    for (let i = 0; i < values.length; i++) {
+       if(values[i] === target)
+        result.push(values[i])
+    }
+    if(result.length === 1) {
+      return 'Target muncul 1 kali'
+    } else if(result.length === 0) {
+      return 'Target tidak ditemukan'
+    }else {
+      return result
+    }
 }
 
 let banana_arr = "banana".split("");
